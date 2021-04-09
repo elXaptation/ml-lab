32b3fa492b5f02a146ad4736113232b3225e1917
elXaptation <el@xaptation>
2021-04-09 10:51:12 -0700

    Update nvidia-smi monitor with native interval

--- nvidia-stuff/dkr-monitor-nvidiasmi.sh
+++ nvidia-stuff/dkr-monitor-nvidiasmi.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-docker run --rm -it --gpus all nvidia/cuda:11.2.1-base /bin/bash -c "watch -d -n5 nvidia-smi"
+docker run --rm -it --gpus all nvidia/cuda:11.2.1-base nvidia-smi -l 2 
 
--- nvidia-stuff/monitor-nvidiasmi.sh
+++ nvidia-stuff/monitor-nvidiasmi.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-watch -d -n 5 -c nvidia-smi
+nvidia-smi -l 2
 
